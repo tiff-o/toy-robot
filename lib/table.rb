012bb1db1b4690a6860d6c599b8b0b709dee04b1
Tiffany Duong <tiff4ny.dev@gmail.com>
2020-10-11 17:28:55 +1100

    updated table initialize args to keywords

--- lib/table.rb
+++ lib/table.rb
@@ -2,7 +2,7 @@ class Table
   attr_reader :width, :length
 
   # constraint: 5 x 5 units
-  def initialize(width = 5, length = 5)
+  def initialize(width: 5, length: 5)
     @width = width
     @length = length
   end
